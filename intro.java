2bdd942334feca13a2b628730427c1bab72631d0
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-13 11:24:27 +0700

    ubah file intro.java

--- intro.java
+++ intro.java
@@ -1,9 +1,11 @@
 public class intro{
     public static void main(String[] args){//menjalankan prosedur
-        hitungLuasSegitiga(15, 4);
-        hitungLuasSegitiga(2, 5);
-        hitungLuasLingkaran(6);
+        intro2.hitungLuasSegitiga(15, 4);
+        intro2.hitungLuasSegitiga(2, 5);
+        intro2.System.out.println(hitungLuasLingkaran(6));
     }
+}
+class intro2{
     //membuat prosedur
     public static void hitungLuasSegitiga(int alas, int tinggi){//ini adalah prosedur alas dan tinggi adalah parameter
         double luas = (alas*tinggi)/2;
@@ -13,5 +15,6 @@ public class intro{
         final double pi = 3.14;
         double luas = r*r*pi;
         return luas;
+    }
     }
 }
