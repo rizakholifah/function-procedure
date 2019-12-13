40448d987b31fd85b46a3e66922974aeeb7737ef
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-13 11:27:41 +0700

    Kompilasi file intro.java

--- intro.java
+++ intro.java
@@ -2,7 +2,7 @@ public class intro{
     public static void main(String[] args){//menjalankan prosedur
         intro2.hitungLuasSegitiga(15, 4);
         intro2.hitungLuasSegitiga(2, 5);
-        intro2.System.out.println(hitungLuasLingkaran(6));
+        System.out.println(intro2.hitungLuasLingkaran(6));
     }
 }
 class intro2{
@@ -15,6 +15,5 @@ class intro2{
         final double pi = 3.14;
         double luas = r*r*pi;
         return luas;
-    }
     }
 }
